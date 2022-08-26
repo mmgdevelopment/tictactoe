da434f897d465335e31564226dc47728683e603b
mmgdevelopment <marcel_gregor@icloud.com>
2022-08-26 19:19:26 +0200

    v2.0

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
 let crossURL = 'url("img/cross.svg")';
 let circleURL = 'url("img/circle.svg")';
-
 let currentPlayer = 'crossPlayer';
-
-let winner = false;
-
+let gameOver = false;
 let crossPlayer;
 let circlePlayer;
+let fields = new Array(9);
 
-let posibleFields = [];
+function init(){
+    crossPlayer = domElemnent('crossPlayer');
+    circlePlayer = domElemnent('circlePlayer');
+    renderCurrentPlayer();
+}
 
-let field1 = '';
-let field2 = '';
-let field3 = '';
-let field4 = '';
-let field5 = '';
-let field6 = '';
-let field7 = '';
-let field8 = '';
-let field9 = '';
-
-let fields = [
-    field1,
-    field2,
-    field3,
-    field4,
-    field5,
-    field6,
-    field7,
-    field8,
-    field9
-];
-
+function domElemnent(id){
+    return document.getElementById(id);
+}
 
 function getRandomInteger(max){
     return Math.floor(Math.random() * max)
-}
-
-function init(){
-    crossPlayer = document.getElementById('crossPlayer');
-    circlePlayer = document.getElementById('circlePlayer');
-    renderCurrentPlayer();
-}
-function domElemnent(id){
-    return document.getElementById(id);
 }
 
 function renderCurrentPlayer(){
@@ -56,26 +30,18 @@ function renderCurrentPlayer(){
     }
 }
 
-function fieldclicked(id) {
-    let field = document.getElementById(id);
-    showPlayerIcon(field);
-    field.style.pointerEvents = 'none';
-    getBackgroundURL();
-    checkForWinner();
-    if (winner == true) {
-        gameFinished('Gewonnen!')
-    };
-    
-    if(winner == false){
+function fieldClicked(id) {
+    let field = domElemnent(id);
+    play(field);
+    if(gameOver == false){
         domElemnent('table').style.pointerEvents = 'none';
         renderCurrentPlayer(); 
         setTimeout(computerPlayer, 500);  
-    }
-
+    };
 };
 
 function computerPlayer(){
-    posibleFields = [];
+    let posibleFields = [];
     for (let i = 0; i < fields.length; i++) {
         const field = fields[i];
         if (field == ''){
@@ -83,23 +49,22 @@ function computerPlayer(){
         }
     }
     index = posibleFields[getRandomInteger(posibleFields.length)];
+    let field = domElemnent(index + 1);
+    play(field);
+    if(gameOver == false){
+        domElemnent('table').style.pointerEvents = 'inherit';
+        renderCurrentPlayer();
+    };
+};
 
-    let field = document.getElementById(index + 1);
-    showPlayerIcon(field);
+function play(field){
+    renderPlayerIcon(field);
     field.style.pointerEvents = 'none';
     getBackgroundURL();
     checkForWinner();
-    if (winner == true) {
-        gameFinished('Gewonnen!')
-    };
-    if(winner == false){
-        domElemnent('table').style.pointerEvents = 'inherit';
-        renderCurrentPlayer();
-    }
-    
 }
 
-function showPlayerIcon(field){
+function renderPlayerIcon(field){
     if (field.style.backgroundImage != crossURL && field.style.backgroundImage != circleURL){
         if (currentPlayer == 'crossPlayer'){
             field.style.backgroundImage = crossURL;
@@ -112,84 +77,93 @@ function showPlayerIcon(field){
     
 };
 
-function checkForWinner(){
-    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[1] != '') {
-        winner = true;
-        domElemnent('1').classList.add('winner');
-        domElemnent('2').classList.add('winner');
-        domElemnent('3').classList.add('winner');
-    } else if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3] != '') {
-        winner = true;
-        domElemnent('4').classList.add('winner');
-        domElemnent('5').classList.add('winner');
-        domElemnent('6').classList.add('winner');
-    } else if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6] != '') {
-        winner = true;
-        domElemnent('7').classList.add('winner');
-        domElemnent('8').classList.add('winner');
-        domElemnent('9').classList.add('winner');
-    } else if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0] != '') {
-        winner = true;
-        domElemnent('1').classList.add('winner');
-        domElemnent('4').classList.add('winner');
-        domElemnent('7').classList.add('winner');
-    } else if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1] != '') {
-        winner = true;
-        domElemnent('2').classList.add('winner');
-        domElemnent('5').classList.add('winner');
-        domElemnent('8').classList.add('winner');
-    } else if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2] != '') {
-        winner = true;
-        domElemnent('3').classList.add('winner');
-        domElemnent('6').classList.add('winner');
-        domElemnent('9').classList.add('winner');
-    } else if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0] != '') {
-        winner = true;
-        domElemnent('1').classList.add('winner');
-        domElemnent('5').classList.add('winner');
-        domElemnent('9').classList.add('winner');
-    } else if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2] != '') {
-        winner = true;
-        domElemnent('3').classList.add('winner');
-        domElemnent('5').classList.add('winner');
-        domElemnent('7').classList.add('winner');
-    } else if (fields[0] != '' && fields[1] != '' && fields[2] != '' && fields[3] != '' &&
-                fields[4] != '' && fields[5] != '' && fields[6] != '' && fields[7] != '' && fields[8] != '' && winner == false){
-                    gameFinished('Unentschieden!');
-                }
-};
-
-function gameFinished(result){
-    let headline = document.getElementById('headline');
-    headline.innerHTML = result;
-    document.getElementById('table').style.pointerEvents = 'none';
-    circlePlayer.style.opacity = '0.1';
-    crossPlayer.style.opacity = '0.1';
-    document.getElementById('button').style.pointerEvents = 'auto';
-    document.getElementById('button').style.opacity = '1';
-
-}
-
 function getBackgroundURL(){
     for (let i = 0; i < fields.length; i++) {
         j = i + 1;
         id = j.toString();
-        fields[i] = document.getElementById(id).style.backgroundImage;
+        fields[i] = domElemnent(id).style.backgroundImage;
     }
+};
+
+function checkForWinner(){
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[1] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('1').classList.add('winner');
+        domElemnent('2').classList.add('winner');
+        domElemnent('3').classList.add('winner');
+    } else if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('4').classList.add('winner');
+        domElemnent('5').classList.add('winner');
+        domElemnent('6').classList.add('winner');
+    } else if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('7').classList.add('winner');
+        domElemnent('8').classList.add('winner');
+        domElemnent('9').classList.add('winner');
+    } else if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('1').classList.add('winner');
+        domElemnent('4').classList.add('winner');
+        domElemnent('7').classList.add('winner');
+    } else if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('2').classList.add('winner');
+        domElemnent('5').classList.add('winner');
+        domElemnent('8').classList.add('winner');
+    } else if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('3').classList.add('winner');
+        domElemnent('6').classList.add('winner');
+        domElemnent('9').classList.add('winner');
+    } else if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('1').classList.add('winner');
+        domElemnent('5').classList.add('winner');
+        domElemnent('9').classList.add('winner');
+    } else if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2] != '') {
+        gameOver = true;
+        gameFinished('Gewonnen!')
+        domElemnent('3').classList.add('winner');
+        domElemnent('5').classList.add('winner');
+        domElemnent('7').classList.add('winner');
+    } else if (fields[0] != '' && fields[1] != '' && fields[2] != '' && fields[3] != '' &&
+                fields[4] != '' && fields[5] != '' && fields[6] != '' && fields[7] != '' && fields[8] != '' && gameOver == false){
+                    gameFinished('Unentschieden!');
+                    gameOver = true;
+                };
+};
+
+function gameFinished(result){
+    let headline = domElemnent('headline');
+    headline.innerHTML = result;
+    domElemnent('table').style.pointerEvents = 'none';
+    circlePlayer.style.opacity = '0.1';
+    crossPlayer.style.opacity = '0.1';
+    domElemnent('button').style.pointerEvents = 'auto';
+    domElemnent('button').style.opacity = '1';
+
 };
 
 function refresh() {
     currentPlayer = 'crossPlayer';
-    winner = false;
+    gameOver = false;
     for (let i = 1; i < 10; i++) {
         id = i.toString();
         domElemnent(id).style.backgroundImage = '';
         domElemnent(id).classList.remove('winner');
         domElemnent(id).style.pointerEvents = 'inherit';
-    }
+    };
     renderCurrentPlayer();
     headline.innerHTML = 'Willkommen!';
-    document.getElementById('table').style.pointerEvents = 'inherit';
-    document.getElementById('button').style.opacity = '0';
-    document.getElementById('button').style.pointerEvents = 'none';
-}
+    domElemnent('table').style.pointerEvents = 'inherit';
+    domElemnent('button').style.opacity = '0';
+    domElemnent('button').style.pointerEvents = 'none';
+};
